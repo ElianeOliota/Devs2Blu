a7566b6977a34d5ea8ed6318526acfc793008860
Eliane Oliota <71183931+ElianeOliota@users.noreply.github.com>
2021-11-25 19:48:33 -0300

    Add files via upload

---
+++ Projeto_jv.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Projeto_jv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("olá! links utéis: ");
+	
+		 ArrayList<String> dados = new ArrayList<String>();
+		    dados.add("https://pt.stackoverflow.com/search");
+		    dados.add("\"https://www.w3schools.com/java/java_getstarted.asp");
+		    dados.add("https://codepen.io/");
+		    dados.add("https://www.devmedia.com.br/principais-atalhos-do-eclipse/25614");
+		    System.out.println(dados);
+	}
+	
+}
---
+++ TipoDadosPrincipal.java
@@ -0,0 +1,15 @@
+
+public class TipoDadosPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String nome = "Eliane Oliota";
+     int idade;
+     char sexo;
+     float salario = 14.99f;
+     double salarioAnual;
+    
+     System.out.println(nome + " -> " + salario);
+	}
+
+}
