65aa9d61f99d9f5c6637469d858e7b31925285e9
Eliane Oliota <71183931+ElianeOliota@users.noreply.github.com>
2021-11-25 21:51:49 -0300

    Add files via upload
    Aula1

--- TipoDadosPrincipal.java
+++ TipoDadosPrincipal.java
@@ -1,15 +1,62 @@
+package Aula1;
+
+import java.sql.Date;
+import java.text.Format;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+
+import javax.swing.JOptionPane;
+
+
 
 public class TipoDadosPrincipal {
 
-	public static void main(String[] args) {
+	private static final TipoDadosPrincipal FloatingDecimal = null;
+
+	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
-     String nome = "Eliane Oliota";
-     int idade;
-     char sexo;
-     float salario = 14.99f;
-     double salarioAnual;
-    
-     System.out.println(nome + " -> " + salario);
+		String nome;
+		int idade;
+		char sexo;
+		float salario;
+		double salarioAnual;
+		java.util.Date dataNascimento;
+
+		//entrada de dados
+		/*
+		 nome = "Eliane Oliota";
+	     idade = 32;
+		 sexo = 'F';
+		 salario = 14.995f;
+		 salarioAnual = 25487.50;
+	
+		-- Para formatar a data de nascimento, podemos utilizar o declaração: 
+		*/
+	SimpleDateFormat formator = new SimpleDateFormat("mm/DD/yyyy");
+		dataNascimento = formator.parse("14/08/1963");
+
+         
+		
+		nome  = JOptionPane.showInputDialog("Informe o nome do sujeito");
+		idade = Integer.parseInt(JOptionPane.showInputDialog( "Informe a idade"));
+		sexo  = JOptionPane.showInputDialog("Informe o sexo").charAt(0);
+		salario = Float.parseFloat(JOptionPane.showInputDialog("Informe o salário mensal"));
+		salarioAnual = Float.parseFloat(JOptionPane.showInputDialog("Informe o salário anual"));
+		dataNascimento = formator.parse(JOptionPane.showInputDialog("Informe a data de nascimento"));
+		String dataNasc = formator.format(dataNascimento);
+	
+		System.out.println(
+				" A " 
+		+ nome + " é do sexo: " 
+		+ sexo + " Data de nascimento : " 
+		+ dataNasc + " Idade : "
+		+ idade + " recebe um salário de : " 
+		+ salario + " Anualmente receber o valor R$: " 
+		+ salarioAnual 
+		);
 	}
 
+	
+	 
 }
