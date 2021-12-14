7f557c1f8b225ad1092887b0ff2343365d034e02
Eliane Oliota <71183931+ElianeOliota@users.noreply.github.com>
2021-12-13 21:53:03 -0300

    add-projetoZoo

---
+++ ProjetoZoo/Classes/ControleAlimentar.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class ControleAlimentar {
+
+    public ControleAlimentar() {
+    }
+
+    public int codAnimal;
+
+    public Timestamp dthrAlimentacao;
+
+    public Timestamp dthrProxAlimentacao;
+
+    public String intercorrencia;
+
+    public void alimentarAnimal() {
+        // TODO implement here
+    }
+
+}
